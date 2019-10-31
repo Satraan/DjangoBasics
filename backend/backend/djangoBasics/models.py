23727059333dc0fc590830b4c91b06d9f9bd6a3d
Mandia <mandiasamuels@gmail.com>
2019-10-31 11:05:07 +0200

    Registered app and added model example

--- backend/backend/djangoBasics/models.py
+++ backend/backend/djangoBasics/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+	
+   class Meta:
+        verbose_name = _('card')
+        verbose_name_plural = _('cards')
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return '{} (#{})'.format(self.title, self.pk)
